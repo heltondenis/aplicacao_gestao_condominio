789905cebb39c6b01fa0f90ec9b9fa78ae24313a
Helton Denis <heltondeniss@gmail.com>
2020-05-10 11:55:27 -0300

    Configuration of the initial modules and their respective pages

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Title } from './styles';
+
+const Dashboard: React.FC = () => {
+    return <Title>Busca de condominios</Title>
+}
+
+export default Dashboard;
+
