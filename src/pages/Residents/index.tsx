71c84392c228970622f4d7a447ed1acf9c488fdf
Helton Denis <heltondeniss@gmail.com>
2020-05-10 15:06:33 -0300

    New pages, Apartaments and Residents

---
+++ src/pages/Residents/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Title } from './styles';
+
+const Residents: React.FC = () => {
+    return <Title>Residentes</Title>
+}
+
+export default Residents;
+
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
-import Repository from '../pages/Repository';
+import Apartaments from '../pages/Apartaments';
+import Residents from '../pages/Residents';
 
 const Routes: React.FC = () => (
     <Switch>
         <Route path="/" exact component={Dashboard} />
-        <Route path="/repository" component={Repository} />
+        <Route path="/apartaments" component={Apartaments} />
+        <Route path="/residents" component={Residents} />
     </Switch>
 )
 
