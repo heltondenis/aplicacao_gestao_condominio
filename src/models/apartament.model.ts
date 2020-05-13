562803002702f936340eea72630af60a702875c9
Helton Denis <heltondeniss@gmail.com>
2020-05-13 20:17:31 -0300

    Add new interfaces

---
+++ src/models/apartament.model.ts
@@ -0,0 +1,8 @@
+export interface Apartament {
+    id: number,
+    number: string,
+    block: string,
+    leased: boolean,
+    created_at: string,
+    updated_at: string
+}
---
+++ src/models/resident.model.ts
@@ -0,0 +1,11 @@
+export interface Resident {
+    id: number,
+    full_name: string,
+    date: Date,
+    cpf: string,
+    fone: string,
+    email: string,
+    admin_status: string,
+    created_at: string,
+    updated_at: string
+}
