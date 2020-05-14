b8544152df86056d5e6367a050aededd8fe708ef
Helton Denis <heltondeniss@gmail.com>
2020-05-14 01:02:13 -0300

    Props errors

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,12 +1,16 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+    hasError: boolean;
+}
 
 export const Title = styled.h1`
     font-size: 28px;
     color: #3a3a3a;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
     margin-top: 40px;
     max-width: 700px;
     display: flex;
@@ -18,6 +22,12 @@ export const Form = styled.form`
         border: 0px;
         border-radius: 5px 0 0 5px;
         color: '#a3a3a3';
+        border: 2px solid #fff;
+        border-right: 0;
+
+        ${(props) => props.hasError && css`
+            border-color: #c53030;
+        `}
 
         &::placeholder{
             color: '#a8a8b3';
@@ -51,6 +61,12 @@ export const Form = styled.form`
             background: ${shade(0.2, '#FF5757')}
         }
     }
+`;
+
+export const Error = styled.span`
+    display: block;
+    color: #c53030;
+    margin-top: 8px;
 `;
 
 export const Feed = styled.div`
@@ -105,3 +121,4 @@ export const Feed = styled.div`
     }
 }
 `;
+
